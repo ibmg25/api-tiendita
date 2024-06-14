0de266b30447916d5bec2eef679ec7e0aa1ff219
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-06-14 11:29:43 -0400

    el index.js ya funciona con las tres tablas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import productsRoutes from './routes/products.routes.js';
+import ordersRoutes from "./routes/orders.routes.js";
+import orderDetailsRoutes from "./routes/orderDetails.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1', productsRoutes);
+app.use('/api-tiendita', productsRoutes);
+app.use('/api-tiendita', ordersRoutes);
+app.use('/api-tiendita', orderDetailsRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor ejecutándose en el puerto 3000');
